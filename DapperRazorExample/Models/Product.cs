a137e26484dac507bba7ea68e8566dbaf0697a1f
Nick Fletcher <nickeax@gmail.com>
2021-09-27 18:41:26 +1000

    Add project files.

---
+++ DapperRazorExample/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperRazorExample.Models
+{
+    public class Product
+    {
+        [Key]
+        [Display(Name = "Product ID")]
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name="Product Name")]
+        [StringLength(25, ErrorMessage = "Name should be 1 to 25 char in lenght")]
+        public string  Name { get; set; }
+
+        [Required]
+        [Display(Name = "Model")]
+        [StringLength(50, ErrorMessage = "Name should be 1 to 50 char in lenght")]
+        public string Model { get; set; }
+
+        [Required]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+    }
+}
